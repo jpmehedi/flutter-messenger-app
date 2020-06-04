21543418cc10747a55b0421ccdc243e9057b0d73
Mehedi Hasan メヘディ ハサン <mehedihasan.cse.jp@gmail.com>
2020-06-04 19:33:13 +0600

    custom animation

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,12 @@ import './screens/registration_screen.dart';
 import './screens/welcome_screen.dart';
 
 void main() => runApp(FlashChat());
-
+ 
 class FlashChat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
         textTheme: TextTheme(
           body1: TextStyle(color: Colors.black54),
